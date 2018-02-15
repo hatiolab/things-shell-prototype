0695600a3d682b0c83600361b91f0e94ddecdcab
Hearty Oh <heartyoh@hatiolab.com>
2018-02-15 14:16:51 +0900

    table editor style

--- src/components/things-editor-table.js
+++ src/components/things-editor-table.js
@@ -33,119 +33,159 @@ class ThingsEditorTable extends mixinBehaviors([IronResizableBehavior], PolymerE
 
   static get template() {
     return html`
-    <style>
+    <style include="shared-styles">
       :host {
         display: block;
         @apply(--things-editor-table)
       }
-      fieldset{
-        padding:0
+
+      fieldset {
+        @apply(--things-property-fieldset);
+        padding: 10px 0 10px 0;
+        margin: 0 0 10px 0;
       }
-      label{
-        @apply(--things-label);
+
+      fieldset legend {
+        @apply(--things-property-fieldset-legend);
       }
-      .icon-only-label{
+
+      .icon-only-label {
         @apply(--things-properties-icon-only-label);
       }
 
-      things-editor-number-input, things-editor-color {
-        width:35%;
+      .property-grid {
+        display: grid;
+        grid-template-columns: repeat(10, 1fr);
+        grid-gap: 5px;
+        grid-auto-rows: minmax(24px, auto);
+        margin: 10px 0 0 0;
       }
 
-      things-editor-color {
-        --things-editor-color-input-text: {
-          width: 100%;
-          margin-bottom: 10px;
-          padding: 3px 5px 2px 5px;
-          -webkit-border-radius: 4px;
-          -moz-border-radius: 4px;
-          border-radius: 4px;
-          border:1px solid rgba(0,0,0,.15);
-          font-size: 14px;
-          font-weight: 300;
-        }
+      .property-grid > * {
+        line-height: 1.5;
       }
-      paper-dropdown-menu::shadow .underline .unfocused-line{background-color:#ccc}
-      paper-dropdown-menu::shadow .underline .focused-line{background-color:var(--secondary-color)}
-      paper-dropdown-menu::shadow input{margin-bottom:0px}
-      #border-set paper-button{
-        margin:0 0 3px 0;
-        padding:5px 2px;
-        min-width:35px;
+
+      #border-set > iron-icon {
+        grid-column: span 2;
+        margin: 0 0 0 8px;
+        width: 32px;
+        height: 32px;
       }
-      .full-width things-editor-color::shadow #detail *{float:none !important}
-      #cell-fieldset{ on-click="_onClickCell"
-        padding:0 10px !important;
+
+      .property-grid > label {
+        grid-column: span 3;
+        text-align: right;
+        text-transform: capitalize;
       }
-      #cell-fieldset  on-click="_onClickCell"paper-button{
-        display:inline-block;
-        width:55px;
-        margin:0 10px 10px 0;
+
+      .property-grid > label.icon-only-label {
+        grid-column: span 1;
       }
+
+      .property-grid > things-editor-color, .property-grid > things-editor-number-input {
+        grid-column: span 4;
+        padding: 0;
+        margin: 0;
+      }
+
+      .property-grid > paper-dropdown-menu {
+        grid-column: span 7;
+        padding: 0;
+        margin: 0;
+      }
+
+      [table-event] {
+        position: relative;
+        background:url('/assets/images/icon-properties-table.png') no-repeat;
+        grid-column: span 2;
+        min-height:65px;
+      }
+
+      [table-event] span {
+        position: absolute;
+        bottom: 0;
+        font-size: 0.9em;
+        line-height: 1.2;
+        text-transform:capitalize;
+        vertical-align:bottom;
+      }
+
+      #merge-cells{background-position:50% 3px}
+      #split-cells{background-position:50% -97px}
+      #delete-row{background-position:50% -197px}
+      #delete-column{background-position:50% -297px}
+      #insert-above{background-position:50% -397px}
+      #insert-below{background-position:50% -497px}
+      #insert-left{background-position:50% -597px}
+      #insert-right{background-position:50% -697px}
+      #distribute-horizontal{background-position:50% -797px}
+      #distribute-vertical{background-position:50% -897px}
+
     </style>
 
     <fieldset id='border-fieldset'>
       <legend>
         <things-i18n-msg msgid="label.border-style">border style</things-i18n-msg>
       </legend>
-      <div id="border-set" class="border-style-btn" on-click="_onClickType">
-        <paper-button data-value="out"><iron-icon icon="editor:border-outer"></iron-icon></paper-button>
-        <paper-button data-value="in"><iron-icon icon="editor:border-inner"></iron-icon></paper-button>
-        <paper-button data-value="all"><iron-icon icon="editor:border-all"></iron-icon></paper-button>
-        <paper-button data-value="left"><iron-icon icon="editor:border-left"></iron-icon></paper-button>
-        <paper-button data-value="center"><iron-icon icon="editor:border-vertical"></iron-icon></paper-button>
-        <paper-button data-value="right"><iron-icon icon="editor:border-right"></iron-icon></paper-button>
-        <paper-button data-value="top"><iron-icon icon="editor:border-top"></iron-icon></paper-button>
-        <paper-button data-value="middle"><iron-icon icon="editor:border-horizontal"></iron-icon></paper-button>
-        <paper-button data-value="bottom"><iron-icon icon="editor:border-bottom"></iron-icon></paper-button>
-        <paper-button data-value="clear"><iron-icon icon="editor:border-clear"></iron-icon></paper-button>
+
+      <div id="border-set" class="property-grid border-style-btn" on-click="_onClickType">
+        <iron-icon data-value="out" icon="editor:border-outer"></iron-icon>
+        <iron-icon data-value="in" icon="editor:border-inner"></iron-icon>
+        <iron-icon data-value="all" icon="editor:border-all"></iron-icon>
+        <iron-icon data-value="left" icon="editor:border-left"></iron-icon>
+        <iron-icon data-value="center" icon="editor:border-vertical"></iron-icon>
+        <iron-icon data-value="right" icon="editor:border-right"></iron-icon>
+        <iron-icon data-value="top" icon="editor:border-top"></iron-icon>
+        <iron-icon data-value="middle" icon="editor:border-horizontal"></iron-icon>
+        <iron-icon data-value="bottom" icon="editor:border-bottom"></iron-icon>
+        <iron-icon data-value="clear" icon="editor:border-clear"></iron-icon>
       </div>
 
-      <div class="full-width">
+      <div class="property-grid">
         <label class="icon-only-label linewidth">
-          <things-i18n-msg msgid="label.size">size</things-i18n-msg>
         </label>
         <things-editor-number-input id="border-width" number="{{borderWidth::change}}">
           <input>
         </things-editor-number-input>
 
         <label class="icon-only-label color">
-          <things-i18n-msg msgid="label.color" >color</things-i18n-msg>
         </label>
         <things-editor-color id="border-color" value="{{borderColor}}">
         </things-editor-color>
-      </div>
 
-      <label>
-        <things-i18n-msg msgid="label.border-type" >border type</things-i18n-msg>
-      </label>
-      <paper-dropdown-menu no-label-float="true" class="line-type solid">
-      <!-- solid는 선택된 항목 보여주기위한 class로 하위 paper-item의 class와 동일하게 -->
-        <paper-listbox id="border-style" slot="dropdown-content" selected="{{borderStyle}}" attr-for-selected="name">
-          <paper-item class="solid" name="solid"></paper-item>
-          <paper-item class="round-dot" name="round-dot"></paper-item>
-          <paper-item class="square-dot" name="square-dot"></paper-item>
-          <paper-item class="dash" name="dash"></paper-item>
-          <paper-item class="dash-dot" name="dash-dot"></paper-item>
-          <paper-item class="long-dash" name="long-dash"></paper-item>
-          <paper-item class="long-dash-dot" name="long-dash-dot"></paper-item>
-          <paper-item class="long-dash-dot-dot" name="long-dash-dot-dot"></paper-item>
-        </paper-listbox>
-      </paper-dropdown-menu>
+        <label>
+          <things-i18n-msg msgid="label.border-type" >border type</things-i18n-msg>
+        </label>
+        <paper-dropdown-menu no-label-float="true" class="line-type solid">
+        <!-- solid는 선택된 항목 보여주기위한 class로 하위 paper-item의 class와 동일하게 -->
+          <paper-listbox id="border-style" slot="dropdown-content" selected="{{borderStyle}}" attr-for-selected="name">
+            <paper-item class="solid" name="solid"></paper-item>
+            <paper-item class="round-dot" name="round-dot"></paper-item>
+            <paper-item class="square-dot" name="square-dot"></paper-item>
+            <paper-item class="dash" name="dash"></paper-item>
+            <paper-item class="dash-dot" name="dash-dot"></paper-item>
+            <paper-item class="long-dash" name="long-dash"></paper-item>
+            <paper-item class="long-dash-dot" name="long-dash-dot"></paper-item>
+            <paper-item class="long-dash-dot-dot" name="long-dash-dot-dot"></paper-item>
+          </paper-listbox>
+        </paper-dropdown-menu>
+      </div>
 
     </fieldset>
 
     <fieldset id="cell-fieldset" on-click="_onClickCell">
-      <paper-button id="merge-cells" table-event>merge cells</paper-button>
-      <paper-button id="split-cells" table-event>split cells</paper-button>
-      <paper-button id="delete-row" table-event>delete row</paper-button>
-      <paper-button id="delete-column" table-event>delete column</paper-button>
-      <paper-button id="insert-above" table-event>insert above</paper-button>
-      <paper-button id="insert-below" table-event>insert below</paper-button>
-      <paper-button id="insert-left" table-event>insert left</paper-button>
-      <paper-button id="insert-right" table-event>insert right</paper-button>
-      <paper-button id="distribute-horizontal" table-event>distribute horizontal</paper-button>
-      <paper-button id="distribute-vertical" table-event>distribute vertical</paper-button>
+      <div class="property-grid">
+        <div id="merge-cells" table-event><span>merge cells</span></div>
+        <div id="split-cells" table-event><span>split cells</span></div>
+        <div id="delete-row" table-event><span>delete row</span></div>
+        <div id="delete-column" table-event><span>delete column</span></div>
+        <div id="insert-above" table-event><span>insert above</span></div>
+        <div id="insert-below" table-event><span>insert below</span></div>
+        <div id="insert-left" table-event><span>insert left</span></div>
+        <div id="insert-right" table-event><span>insert right</span></div>
+        <div id="distribute-horizontal" table-event><span>distribute horizontal</span></div>
+        <div id="distribute-vertical" table-event><span>distribute vertical</span></div>
+      </div>
     </fieldset>
   `;
   }
