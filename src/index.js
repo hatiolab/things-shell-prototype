694635d0b9af68e8964c443ae6a953b70a2fc263
Hearty Oh <heartyoh@hatiolab.com>
2018-02-25 01:21:06 +0900

    0.2.17

--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ import './styles/shared-styles';
 import './app-shell/app-shell';
 
 import './things-scene-components.import';
-import component_metas from './things-scene-components-with-tools.import';
-
-console.log('modules', component_metas);
