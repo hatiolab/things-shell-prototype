0cc050255fce0fbe5cffddb2ef36da03a025c3f0
Hearty Oh <heartyoh@hatiolab.com>
2018-02-26 21:32:06 +0900

    0.2.23

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,7 @@
 const boardRoutes = require('./board-routes');
 const groupRoutes = require('./group-routes');
-const playGroupRoutes = require('./play-group-routes');
 
 module.exports = function (app, db) {
   boardRoutes(app, db);
   groupRoutes(app, db);
-  playGroupRoutes(app, db);
 };
