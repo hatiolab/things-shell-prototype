cac3551449649ea39ae34ad8c08294a4e4d1eea1
Hearty Oh <heartyoh@hatiolab.com>
2018-02-26 23:23:55 +0900

    0.2.24

--- src/reducer/reducer-component.js
+++ src/reducer/reducer-component.js
@@ -1317,24 +1317,6 @@ function dataSource() {
       dataFormat: 'json'
     }
   }, {
-    type: 'firebase',
-    model: {
-      type: 'firebase',
-      top: 50,
-      left: 50,
-      width: 100,
-      height: 100,
-      apiKey: "AIzaSyBtJayCKxuU-_lPaZvbLmOgqFlynMIu_sM",
-      authDomain: "things-rtls.firebaseapp.com",
-      databaseURL: "https://things-rtls.firebaseio.com",
-      projectId: "things-rtls",
-      storageBucket: "things-rtls.appspot.com",
-      messagingSenderId: "32358989541",
-      childDataPath: "boards/template/data",
-      email: 'test@example.com',
-      password: 'testpass'
-    }
-  }, {
     type: 'restful',
     model: {
       type: "restful",
@@ -1683,145 +1665,6 @@ function warehouse() {
   }]
 };
 
-function form() {
-
-  var inputs01 = ['text', 'password', 'email', 'search', 'number', 'color', 'range', 'file', 'date'].map(function (type) {
-    return {
-      type: 'input-' + type,
-      model: {
-        type: 'input-' + type,
-        top: 100,
-        left: 100,
-        width: 280,
-        height: 30,
-        paddingLeft: type == 'search' ? 0 : 7,
-        paddingRight: type == 'search' ? 0 : 7,
-        fontSize: 14,
-        fillStyle: 'white',
-        fontColor: '#585858',
-        strokeStyle: 'rgba(0,0,0,.4)',
-        lineWidth: type == 'file' ? 0 : 1,
-        lineDash: 'solid',
-        textAlign: 'left'
-      }
-    }
-  })
-
-  var inputs02 = ['submit', 'reset'].map(function (type) {
-    return {
-      type: 'input-' + type,
-      model: {
-        type: 'input-' + type,
-        top: 100,
-        left: 100,
-        width: 280,
-        height: 30,
-        fontSize: 14,
-        fillStyle: 'white',
-        fontColor: '#585858',
-        strokeStyle: 'rgba(0,0,0,.4)'
-      }
-    }
-  })
-
-  var button = ['button'].map(function (type) {
-    return {
-      type: type,
-      model: {
-        type: type,
-        top: 100,
-        left: 100,
-        width: 280,
-        height: 30,
-        fontSize: 14,
-        fillStyle: 'white',
-        fontColor: '#585858',
-        textAlign: 'center'
-      }
-    }
-  })
-
-  var textibles = ['input-radio', 'input-checkbox'].map(function (type) {
-    return {
-      type: type,
-      model: {
-        type: type,
-        top: 100,
-        left: 100,
-        width: 280,
-        height: 30,
-        text: 'noname',
-        fontSize: 14,
-        fontColor: '#585858',
-        textAlign: 'left'
-      }
-    }
-  })
-
-  var fieldset = ['fieldset', 'iframe', 'img', 'link'].map(function (type) {
-    return {
-      type: type,
-      model: {
-        type: type,
-        top: 100,
-        left: 100,
-        width: 280,
-        height: 30,
-        fontSize: 14,
-        fillStyle: 'white',
-        fontColor: '#585858',
-        strokeStyle: 'rgba(0,0,0,.4)',
-        lineWidth: 1,
-        lineDash: 'solid',
-        textAlign: 'left'
-      }
-    }
-  })
-
-  var others = ['textarea', 'select'].map(function (type) {
-    return {
-      type: type,
-      model: {
-        type: type,
-        top: 100,
-        left: 100,
-        width: 280,
-        height: type == 'textarea' ? 60 : 40,
-        paddingLeft: type == 'select' ? 0 : 7,
-        paddingRight: type == 'select' ? 0 : 7,
-        fontSize: 14,
-        fillStyle: 'white',
-        fontColor: '#585858',
-        strokeStyle: 'rgba(0,0,0,.4)',
-        lineWidth: 1,
-        lineDash: 'solid',
-        textAlign: 'left'
-      }
-    }
-  })
-
-  var form = [{
-    type: 'form',
-    model: {
-      type: 'form',
-      top: 100,
-      left: 100,
-      width: 400,
-      height: 200,
-      fontColor: '#585858',
-      strokeStyle: '#ccc',
-      lineWidth: 1,
-      method: 'GET',
-      action: '',
-      name: 'search',
-      authorization: '',
-      format: 'TEXT'
-    }
-  }]
-
-  return form.concat(inputs01, inputs02, button, textibles, fieldset, others)
-};
-
 function etc() {
   return [{
     type: 'info. window',
@@ -1975,7 +1818,7 @@ const STATE = {
     IoT: IoTs(),
     '3D': threeD(),
     warehouse: warehouse(),
-    form: form(),
+    form: [],
     etc: etc()
   }
 };
